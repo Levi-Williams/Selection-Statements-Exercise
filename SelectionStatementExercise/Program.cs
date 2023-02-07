a7301c506d2e86a36f9ab16d0bec28a2c6cc2932
Levi Williams <ljw758@hotmail.com>
2023-02-07 17:11:20 -0600

    Completed Exercise 1

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -4,7 +4,50 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            // EXERCISE 1 //
+
+            int favNumber = 15;
+            Console.WriteLine("Guess my favorite number between 1 and 100");
+            int userGuess = 0;
+
+             string GuessingGame()
+            {
+                var userGuess = int.Parse(Console.ReadLine());
+
+                if (userGuess == 015)
+                {
+                    Console.WriteLine("Wow! You guessed it right. Good job!");
+
+                }
+                else if (userGuess > 0 && userGuess < 15) 
+                {
+                    Console.WriteLine("Your need to guess a higher number!");
+                }
+
+                else if (userGuess > 15)
+                {
+                    Console.WriteLine("You need to guess a lower number!");
+                }
+
+                else
+                {
+                    Console.WriteLine("That is a value I don't recognize!");
+                }
+
+                return  "To beginning";
+               
+            }
+
+            GuessingGame();
+
+            do
+            {
+                GuessingGame();
+            } while (userGuess != 15 );
+
+
+
         }
     }
 }
